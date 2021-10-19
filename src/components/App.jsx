6542f7cffcb693c561ecb43e9eb40ce0dec0175f
melvinrajendran <melvin.rajendran@gmail.com>
2021-10-19 01:00:38 -0400

    Add custom 404 page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Navigation from "./common/Navigation";
 import About from "./about/About";
 import Work from "./work/Work";
 import BTW from "./btw/BTW";
+import NotFound from "./common/NotFound";
 import Footer from "./common/Footer";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route exact path="/btw">
           <BTW />
+        </Route>
+        <Route exact path="*">
+          <NotFound />
         </Route>
       </Switch>
       <Footer />
---
+++ src/components/common/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NotFound.css";
+import { Col, Container, Row } from "react-bootstrap";
+
+function NotFound() {
+  return (
+    <Container fluid className="p-4">
+      <Container>
+        <Row className="not-found-container align-items-center">
+          <Col className="text-center py-5">
+            <h1 className="display-1 mb-4">404!</h1>
+            <p className="fs-3">The page you're looking for does not exist.</p>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default NotFound;
