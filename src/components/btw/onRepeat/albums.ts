fe289983fc5a220fa5c8a0ff2dd574d1124fc1d8
melvinrajendran <melvin.rajendran@gmail.com>
2022-08-14 12:42:33 -0400

    Update OnRepeat albums

--- src/components/btw/onRepeat/albums.ts
+++ src/components/btw/onRepeat/albums.ts
@@ -1,24 +1,24 @@
 const albums = [
   {
-    songURL: 'https://open.spotify.com/album/7KtyUeiJidoZO0ybxBXw0Q?si=GgCrAbWFSS681iJWgKV-tg',
-    imageURL: '/images/music/denzel-melt.png',
-    imageAlt: 'Melt My Eyez See Your Future',
-    albumTitle: 'Melt My Eyez See Your Future',
-    artist: 'Denzel Curry'
-  },
-  {
-    songURL: 'https://open.spotify.com/album/6UMelDEJAzDUlgUi9VxPFf?si=ConvG1CUSSGY5-vgwtIYfg',
-    imageURL: '/images/music/redveil-learn.png',
-    imageAlt: 'learn 2 swim',
-    albumTitle: 'learn 2 swim',
-    artist: 'redveil'
-  },
-  {
     songURL: 'https://open.spotify.com/album/1atjqOZTCdrjxjMyCPZc2g?si=sS1B6NdAThulrM6gAjM9WQ',
     imageURL: '/images/music/kendrick-mr.jpg',
     imageAlt: 'Mr. Morale & The Big Steppers',
     albumTitle: 'Mr. Morale & The Big Steppers',
     artist: 'Kendrick Lamar'
+  },
+  {
+    songURL: 'https://open.spotify.com/album/0PHMNbcgHfzSUALlfk7wGg?si=yimK4MgXTduQiWEW7Dgrvg',
+    imageURL: '/images/music/brent-wasteland.jpeg',
+    imageAlt: 'WASTELAND',
+    albumTitle: 'WASTELAND',
+    artist: 'Brent Faiyaz'
+  },
+  {
+    songURL: 'https://open.spotify.com/album/261LWEVYViMXijB95mAty3?si=IIKhB2U4SU-7lOlnrwOvPg',
+    imageURL: '/images/music/joey-2000.jpeg',
+    imageAlt: '2000',
+    albumTitle: '2000',
+    artist: 'Joey Bada$$'
   }
 ];
 
