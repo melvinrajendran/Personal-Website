4df79e574dfa71163b82ca17e8216f54cc54e73e
melvinrajendran <melvin.rajendran@gmail.com>
2022-08-19 11:41:12 -0400

    Animate AboutMe section

---
+++ src/components/AboutMeItem/AboutMeItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
+import { CSSTransition } from 'react-transition-group';
+import './AboutMeItem.scss';
+
+type AboutMeItemProps = {
+  index: number;
+  text: string;
+  iconClass: string;
+};
+
+const AboutMeItem = ({ index, text, iconClass }: AboutMeItemProps) => {
+  const { ref, inView } = useInView({
+    threshold: 1,
+    triggerOnce: true,
+  });
+  const hasEvenIndex = index % 2 === 0;
+
+  return (
+    <Row key={index}>
+      <CSSTransition in={inView} timeout={1000} classNames="about-me-item">
+        <Col
+          lg
+          className={`${!inView && 'invisible'} mb-4 text-center ${
+            hasEvenIndex ? 'order-lg-1' : 'order-lg-2'
+          }`}
+          ref={ref}
+        >
+          <p className="p-4 h2 fw-normal">{text}</p>
+        </Col>
+      </CSSTransition>
+      <Col lg className={`mb-4 text-center ${hasEvenIndex ? 'order-lg-2' : 'order-lg-1'}`}>
+        <i className={`about-icon ${iconClass}`} />
+      </Col>
+    </Row>
+  );
+};
+
+export default AboutMeItem;
--- src/components/AboutMeList/AboutMeList.tsx
+++ src/components/AboutMeList/AboutMeList.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import AboutMeItem from '../AboutMeItem/AboutMeItem';
 import './AboutMeList.scss';
 
-type AboutMeListItem = {
+type AboutMeItem = {
   text: string;
   iconClass: string;
 };
 
 type AboutMeListProps = {
-  items: AboutMeListItem[];
+  items: AboutMeItem[];
 };
 
 const AboutMeList = ({ items }: AboutMeListProps) => {
   return (
     <>
       {items.map((item, index) => {
-        const { text, iconClass } = item,
-          hasEvenIndex = index % 2 === 0;
-
-        return (
-          <Row key={index}>
-            <Col lg className={`mb-4 text-center ${hasEvenIndex ? 'order-lg-1' : 'order-lg-2'}`}>
-              <p className="p-4 h2 fw-normal">{text}</p>
-            </Col>
-            <Col lg className={`mb-4 text-center ${hasEvenIndex ? 'order-lg-2' : 'order-lg-1'}`}>
-              <i className={`about-icon ${iconClass}`} />
-            </Col>
-          </Row>
-        );
+        return <AboutMeItem key={index} index={index} {...item} />;
       })}
     </>
   );
