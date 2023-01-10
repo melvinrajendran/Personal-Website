555cba50d90edff1af545abbf95acc9d4da877db
melvinrajendran <melvin.rajendran@gmail.com>
2023-01-09 20:35:01 -0500

    Update animations on BTW page

--- src/components/AlbumCard/AlbumCard.tsx
+++ src/components/AlbumCard/AlbumCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
+import { CSSTransition } from 'react-transition-group';
 import './AlbumCard.scss';
 
 type AlbumCardProps = {
@@ -11,18 +13,25 @@ type AlbumCardProps = {
 };
 
 const AlbumCard = ({ albumURL, imageURL, imageAlt, title, artist }: AlbumCardProps) => {
+  const { ref, inView } = useInView({
+    threshold: 1,
+    triggerOnce: true,
+  });
+
   return (
-    <Card className="album-card">
-      <a href={albumURL} target="_blank" rel="noopener noreferrer">
-        <Card.Img className="rounded shadow" variant="top" src={imageURL} alt={imageAlt} />
-      </a>
-      <Card.Body className="pt-4">
-        <Card.Title>
-          <h3 className="h5">{title}</h3>
-        </Card.Title>
-        <Card.Text>{artist}</Card.Text>
-      </Card.Body>
-    </Card>
+    <CSSTransition in={inView} timeout={1000} classNames="album">
+      <Card ref={ref} className={`${!inView && 'invisible'} album-card`}>
+        <a href={albumURL} target="_blank" rel="noopener noreferrer">
+          <Card.Img className="rounded shadow" variant="top" src={imageURL} alt={imageAlt} />
+        </a>
+        <Card.Body className="pt-4">
+          <Card.Title>
+            <h3 className="h5">{title}</h3>
+          </Card.Title>
+          <Card.Text>{artist}</Card.Text>
+        </Card.Body>
+      </Card>
+    </CSSTransition>
   );
 };
 
