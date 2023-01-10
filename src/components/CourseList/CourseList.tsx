033ffe623c8ebb56f115b774c1077dbb692f95a2
melvinrajendran <melvin.rajendran@gmail.com>
2023-01-09 20:50:10 -0500

    Remove unnecessary animations

--- src/components/CourseList/CourseList.tsx
+++ src/components/CourseList/CourseList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
+import { CSSTransition } from 'react-transition-group';
 import './CourseList.scss';
 
 type Course = {
@@ -12,23 +14,30 @@ type CourseListProps = {
 };
 
 const CourseList = ({ items }: CourseListProps) => {
-  return (
-    <Row className="p-4 justify-content-center">
-      <Col>
-        <ul className="h2 fw-normal coursework-list text-center">
-          {items.map((course, index) => {
-            const { code, name } = course;
+  const { ref, inView } = useInView({
+    threshold: 0.75,
+    triggerOnce: true,
+  });
 
-            return (
-              <li key={index} className="py-3">
-                <h3 className="course-name">{name}</h3>
-                <p className="h6 fw-normal course-code">{code}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </Col>
-    </Row>
+  return (
+    <CSSTransition in={inView} timeout={1000} classNames="course-list">
+      <Row ref={ref} className={`${!inView && 'invisible'} p-4 justify-content-center`}>
+        <Col>
+          <ul className="h2 fw-normal coursework-list text-center">
+            {items.map((course, index) => {
+              const { code, name } = course;
+
+              return (
+                <li key={index} className="py-3">
+                  <h3 className="course-name">{name}</h3>
+                  <p className="h6 fw-normal course-code">{code}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </Col>
+      </Row>
+    </CSSTransition>
   );
 };
 
