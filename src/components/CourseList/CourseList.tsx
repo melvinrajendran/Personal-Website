478c0adbd0466ee1b672bd7ebb7b1a77fc28c54d
melvinrajendran <melvin.rajendran@gmail.com>
2024-04-24 22:10:11 -0400

    Update course list

--- src/components/CourseList/CourseList.tsx
+++ src/components/CourseList/CourseList.tsx
@@ -18,24 +18,33 @@ const CourseList = ({ items }: CourseListProps) => {
     threshold: 0.5,
     triggerOnce: true,
   });
+  const numItems = items.length;
+  const midIndex = numItems % 2 === 0 ? numItems / 2 : numItems / 2 + 1;
 
   return (
     <CSSTransition in={inView} timeout={1000} classNames="course-list">
       <Row ref={ref} className={`${!inView && 'invisible'} p-4 justify-content-center`}>
-        <Col>
-          <ul className="h2 fw-normal coursework-list text-center">
-            {items.map((course, index) => {
-              const { code, name } = course;
+        {[0, 1].map((index) => {
+          const startIndex = index === 0 ? 0 : midIndex;
+          const endIndex = index === 0 ? midIndex : numItems;
 
-              return (
-                <li key={index} className="py-3">
-                  <h3 className="course-name">{name}</h3>
-                  <p className="h6 fw-normal course-code">{code}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </Col>
+          return (
+            <Col key={index} lg={{ span: 5, offset: index === 0 ? 0 : 1 }}>
+              <ul className="h2 fw-normal coursework-list text-center">
+                {items.slice(startIndex, endIndex).map((course, index) => {
+                  const { code, name } = course;
+
+                  return (
+                    <li key={index} className="py-3">
+                      <h3 className="course-name">{name}</h3>
+                      <p className="h6 fw-normal course-code">{code}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Col>
+          );
+        })}
       </Row>
     </CSSTransition>
   );
--- src/components/CourseList/courses.ts
+++ src/components/CourseList/courses.ts
@@ -5,6 +5,30 @@ type Course = {
 
 const courses: Array<Course> = [
   {
+    code: 'CMSC131 & CMSC132',
+    name: 'Object-Oriented Programming',
+  },
+  {
+    code: 'CMSC216',
+    name: 'Computer Systems',
+  },
+  {
+    code: 'CMSC250',
+    name: 'Discrete Structures',
+  },
+  {
+    code: 'CMSC320',
+    name: 'Data Science',
+  },
+  {
+    code: 'CMSC330',
+    name: 'Programming Languages',
+  },
+  {
+    code: 'CMSC351',
+    name: 'Algorithms',
+  },
+  {
     code: 'CMSC416',
     name: 'Parallel Computing',
   },
@@ -25,40 +49,16 @@ const courses: Array<Course> = [
     name: 'Compilers',
   },
   {
-    code: 'CMSC351',
-    name: 'Algorithms',
-  },
-  {
-    code: 'CMSC320',
-    name: 'Data Science',
-  },
-  {
-    code: 'CMSC330',
-    name: 'Programming Languages',
-  },
-  {
-    code: 'CMSC216',
-    name: 'Computer Systems',
-  },
-  {
-    code: 'CMSC250',
-    name: 'Discrete Structures',
-  },
-  {
-    code: 'CMSC131 & CMSC132',
-    name: 'Object-Oriented Programming',
-  },
-  {
-    code: 'STAT400',
-    name: 'Applied Probability and Statistics',
+    code: 'MATH140 & MATH141',
+    name: 'Calculus',
   },
   {
     code: 'MATH240',
     name: 'Linear Algebra',
   },
   {
-    code: 'MATH140 & MATH141',
-    name: 'Calculus',
+    code: 'STAT400',
+    name: 'Applied Probability and Statistics',
   },
 ];
 
